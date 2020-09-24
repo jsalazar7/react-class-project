17667eabb94e18a7b3d80f46189097ece621ae93
Jorge Salazar <jorge_salazar@intuit.com>
2020-09-24 10:01:38 -0600

    fix for edit

--- vote-app/src/components/CarEditRow.tsx
+++ vote-app/src/components/CarEditRow.tsx
@@ -23,7 +23,7 @@ export type CarEditRowProps = {
 export function CarEditRow({car, onSaveCar, onCancelCar}: CarEditRowProps) {
 
     const [ carForm, change ] = useForm ({
-        firstName: '', lastName: '', address: '', city: '', birthday: 0, email: '', phone: 0,
+       ...car
     }); /* initial value used to initialize the state on the first render */
 
     const saveCar = () => {
@@ -35,13 +35,13 @@ export function CarEditRow({car, onSaveCar, onCancelCar}: CarEditRowProps) {
     return (
         <tr>
             <td>{car.id}</td>
-            <td><input type="text" name="first" value={car.firstName} onChange={change} /></td>
-            <td><input type="text" name="last" value={car.lastName} onChange={change} /></td>
-            <td><input type="text" name="address" value={car.address} onChange={change} /></td>
-            <td><input type="text" name="city" value={car.city} onChange={change} /></td>
-            <td><input type="number" name="birthday" value={car.birthday} onChange={change} /></td>
-            <td><input type="text" name="email" value={car.email} onChange={change} /></td>
-            <td><input type="number" name="phone" value={car.phone} onChange={change} /></td>
+            <td><input type="text" name="first" value={carForm.firstName} onChange={change} /></td>
+            <td><input type="text" name="last" value={carForm.lastName} onChange={change} /></td>
+            <td><input type="text" name="address" value={carForm.address} onChange={change} /></td>
+            <td><input type="text" name="city" value={carForm.city} onChange={change} /></td>
+            <td><input type="number" name="birthday" value={carForm.birthday} onChange={change} /></td>
+            <td><input type="text" name="email" value={carForm.email} onChange={change} /></td>
+            <td><input type="number" name="phone" value={carForm.phone} onChange={change} /></td>
             <td>
                 <button type="button"
                         onClick={(saveCar)}>Save</button>
