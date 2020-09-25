36eafa796cb380182ffa9f41bd7cf42929efd3ec
br7545 <babraju@hotmail.com>
2020-09-25 09:05:42 -0700

    Added team-logo

--- vote-app/src/components/ToolHeader.tsx
+++ vote-app/src/components/ToolHeader.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './ToolHeader.css';
 import {Link} from "react-router-dom";
+import logo from '../assets/team-logo.png';
 
 export type ToolHeaderProps = {
     headerText: string,
@@ -13,10 +14,13 @@ export function ToolHeader(props: ToolHeaderProps) {
         <header>
             <div id="link">
             <Link to={"/"}>Home</Link>
+            
             </div>
+            <img src={logo} alt="Logo" />;
             <Jumbotron>
                 <h1>{props.headerText}</h1>
             </Jumbotron>
+            
         </header>
     );
 }
