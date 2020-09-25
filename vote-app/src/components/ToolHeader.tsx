f069732519b84c56a30462938d6daf6fe2cdb121
Beatty <annette_beatty@intuit.com>
2020-09-24 19:47:28 -0700

    Added heading styling

--- vote-app/src/components/ToolHeader.tsx
+++ vote-app/src/components/ToolHeader.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 export type ToolHeaderProps = {
-   headerText: string,
+    headerText: string,
 }
 
 export function ToolHeader(props: ToolHeaderProps) {
 
-   return  (
-       <header>
-          <h1>{props.headerText}</h1>
-       </header>
-   );
+    return  (
+        <header>
+            <Jumbotron>
+                <h1>{props.headerText}</h1>
+            </Jumbotron>
+        </header>
+    );
 }
