9a142651037a6122355e914ebc5890ec27c2bf94
Beatty <annette_beatty@intuit.com>
2020-09-24 22:11:35 -0700

    Added /home link in the header

--- vote-app/src/components/ToolHeader.tsx
+++ vote-app/src/components/ToolHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './ToolHeader.css';
+import {Link} from "react-router-dom";
 
 export type ToolHeaderProps = {
     headerText: string,
@@ -8,11 +9,16 @@ export type ToolHeaderProps = {
 
 export function ToolHeader(props: ToolHeaderProps) {
 
-    return  (
+    return (
         <header>
+            <div id="link">
+            <Link to={"/"}>Home</Link>
+            </div>
             <Jumbotron>
                 <h1>{props.headerText}</h1>
             </Jumbotron>
         </header>
     );
 }
+
+
