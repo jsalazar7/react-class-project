81ddd5f47da8161152959e6591d17a33c22f10b5
Beatty <annette_beatty@intuit.com>
2020-09-25 06:52:04 -0700

    Added CSS to home page

--- vote-app/src/components/Home.tsx
+++ vote-app/src/components/Home.tsx
@@ -1,6 +1,7 @@
 import {ToolHeader} from "./ToolHeader";
 import React from "react";
 import { Link } from "react-router-dom";
+import './voters/VoterTool.css'
 
 export function Home() {
 
@@ -9,11 +10,13 @@ export function Home() {
           <ToolHeader headerText="Election Home Page" />
           <div>
             <nav>
+            <form>
                 <ul>
                     <li><Link to={"/voter"}>Voter Registration</Link></li>
                     <li><Link to={"/createElections"}>Create Elections</Link></li>
                     <li><Link to={"/elections"}>Select Election</Link></li>
                 </ul>
+            </form>
             </nav>
           </div>
       </>
